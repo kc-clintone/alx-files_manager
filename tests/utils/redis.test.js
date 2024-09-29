d236ca9d0a21d9ae1a056bceaf193c255fe36e4c
kc-clintone <kayseeclintone@gmail.com>
2024-09-29 10:42:12 +0000

    working on tests

--- tests/utils/redis.test.js
+++ tests/utils/redis.test.js
@@ -4,27 +4,27 @@ import redisClient from '../../utils/redis';
 
 describe('+ RedisClient utility', () => {
   before(function (done) {
-    this.timeout(10000);
-    setTimeout(done, 4000);
+    this.timeout(10500);
+    setTimeout(done, 5000);
   });
 
-  it('+ Client is alive', () => {
+  it('+ Client is live', () => {
     expect(redisClient.isAlive()).to.equal(true);
   });
 
-  it('+ Setting and getting a value', async function () {
+  it('+ Setting and getting values', async function () {
     await redisClient.set('test_key', 345, 10);
     expect(await redisClient.get('test_key')).to.equal('345');
   });
 
-  it('+ Setting and getting an expired value', async function () {
+  it('+ Setting and getting expired value', async function () {
     await redisClient.set('test_key', 356, 1);
     setTimeout(async () => {
       expect(await redisClient.get('test_key')).to.not.equal('356');
     }, 2000);
   });
 
-  it('+ Setting and getting a deleted value', async function () {
+  it('+ Setting and getting deleted values', async function () {
     await redisClient.set('test_key', 345, 10);
     await redisClient.del('test_key');
     setTimeout(async () => {
