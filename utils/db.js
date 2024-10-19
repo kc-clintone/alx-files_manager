087d332974c09b3894fee5e864b018fe31564808
kc-clintone <kayseeclintone@gmail.com>
2024-10-19 13:44:00 +0300

    corrections

--- utils/db.js
+++ utils/db.js
@@ -4,8 +4,11 @@ import Collection from 'mongodb/lib/collection';
 import loadEnv from './loadEnv';
 
 class DBClient {
+  /**
+   * Creates a new DBClient instance.
+   */
   constructor() {
-    loadEnv();
+    Loadenv();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
@@ -15,21 +18,42 @@ class DBClient {
     this.client.connect();
   }
 
+  /**
+   * Checks for active mongoDB connection
+   * @returns {boolean}
+   */
   isAlive() {
     return this.client.isConnected();
   }
 
+  /**
+   * Retrieves users from database.
+   * @returns {Promise<Number>}
+   */
   async nbUsers() {
     return this.client.db().collection('users').countDocuments();
   }
+
+  /**
+   * Retrieves files from the database.
+   * @returns {Promise<Number>}
+   */
   async nbFiles() {
     return this.client.db().collection('files').countDocuments();
   }
 
+  /**
+   * Gets reference to `users` collection.
+   * @returns {Promise<Collection>}
+   */
   async usersCollection() {
     return this.client.db().collection('users');
   }
 
+  /**
+   * Gets reference to `files` collection.
+   * @returns {Promise<Collection>}
+   */
   async filesCollection() {
     return this.client.db().collection('files');
   }
