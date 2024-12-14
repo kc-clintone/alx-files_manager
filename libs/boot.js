2a816fd43328f66548a0077bf506fedacb49aecc
kaysee clintone <kayseeclintone@gmail.com>
2024-12-14 15:18:33 +0300

    Update boot.js

--- libs/boot.js
+++ libs/boot.js
@@ -1,6 +1,7 @@
 import loadEnv from '../utils/env_loader';
 
-const startServer = (api) => {                                                  envLoader();
+const startServer = (api) => {
+  envLoader();
   const port = process.env.PORT || 5000;
   const env = process.env.npm_lifecycle_event || 'dev';
   api.listen(port, () => {
